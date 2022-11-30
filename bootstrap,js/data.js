869816c1f5c96214230956ef4b52d814ccaeb4b6
Aptech <Aptech@gamil.com>
2022-11-30 10:48:36 +0500

    This is my first commit

---
+++ bootstrap,js/data.js
@@ -0,0 +1,23 @@
+var Products = [ 
+    {"ID":0 ,"Name":"","Category":"","Price":"","Image":"h","Specification":""},
+    {"ID":1 ,"Name":"Honda","Category":"city","":"","Image":"1.jpg","Specification":"Honda was the first Japanese automobile manufacturer to release a dedicated luxury brand, Acura, in 1986. Aside from their core automobile and motorcycle businesses,"},
+    {"ID":2 ,"Name":"Honda","Category":"civik","":"","Image":"2.gif","Specification":"Honda was the first Japanese automobile manufacturer to release a dedicated luxury brand, Acura, in 1986. Aside from their core automobile and motorcycle businesses,"},
+    {"ID":3 ,"Name":"Honda","Category":"AMAZE","":"","Image":"3.jpg","Specification":"Honda was the first Japanese automobile manufacturer to release a dedicated luxury brand, Acura, in 1986. Aside from their core automobile and motorcycle businesses,"},
+    {"ID":4 ,"Name":"Honda","Category":"BRV","":"","Image":"4.png","Specification":"Badminton is a type of indoor game which is played between either two teams or two players. It is one of the best games for an individual's physical fitness,"},
+    {"ID":5 ,"Name":"Honda","Category":"iNSPIRE","":"","Image":"5.jpg","Specification":"Honda was the first Japanese automobile manufacturer to release a dedicated luxury brand, Acura, in 1986. Aside from their core automobile and motorcycle businesses,"},
+    {"ID":6,"Name":"Honda","Category":"ACCORD","":"","Image":"6.jpg","Specification":"Honda was the first Japanese automobile manufacturer to release a dedicated luxury brand, Acura, in 1986. Aside from their core automobile and motorcycle businesses,"},
+    {"ID":7 ,"Name":"TOYOTA","Category":"COROLLA","":"","Image":"7.png","Specification":"Honda was the first Japanese automobile manufacturer to release a dedicated luxury brand, Acura, in 1986. Aside from their core automobile and motorcycle businesses,"},
+    {"ID":8 ,"Name":"TOYOTA","Category":"CROSS","":"","Image":"8.jpg","Specification":"Honda was the first Japanese automobile manufacturer to release a dedicated luxury brand, Acura, in 1986. Aside from their core automobile and motorcycle businesses,"},
+    {"ID":9 ,"Name":"TOYOTA","Category":"4RUNNER","":"","Image":"9.webp","Specification":"Honda was the first Japanese automobile manufacturer to release a dedicated luxury brand, Acura, in 1986. Aside from their core automobile and motorcycle businesses,"},
+    {"ID":10 ,"Name":"TOYOTA","Category":"ALTIS","":"","Image":"10.jpg","Specification":"Honda was the first Japanese automobile manufacturer to release a dedicated luxury brand, Acura, in 1986. Aside from their core automobile and motorcycle businesses,"},
+    {"ID":11 ,"Name":"TOYOTA","Category":"VIOS","":"","Image":"11.webp","Specification":"Honda was the first Japanese automobile manufacturer to release a dedicated luxury brand, Acura, in 1986. Aside from their core automobile and motorcycle businesses,"},
+    {"ID":12,"Name":"TOYOTA","Category":"CROWN","":"","Image":"12.png","Specification":"Honda was the first Japanese automobile manufacturer to release a dedicated luxury brand, Acura, in 1986. Aside from their core automobile and motorcycle businesses,"},
+]
+
+
+
+
+
+
+
+
---
+++ data.js
@@ -0,0 +1,23 @@
+var Products = [ 
+    {"ID":0 ,"Name":"","Category":"","Price":"","Image":"h","Specification":"","price":""},
+    {"ID":1 ,"Name":"Honda","Category":"city","":"","Image":"img.img2.jpg","Specification":"Honda was the first Japanese automobile manufacturer to release a dedicated luxury brand, Acura, in 1986. Aside from their core automobile and motorcycle businesses,","price":"$4500"},
+    {"ID":2 ,"Name":"Honda","Category":"civik","":"","Image":"img.img3.jpg","Specification":"Honda was the first Japanese automobile manufacturer to release a dedicated luxury brand, Acura, in 1986. Aside from their core automobile and motorcycle businesses,","price":"$4500"},
+    {"ID":3 ,"Name":"Honda","Category":"AMAZE","":"","Image":"img.img.4.jpg","Specification":"Honda was the first Japanese automobile manufacturer to release a dedicated luxury brand, Acura, in 1986. Aside from their core automobile and motorcycle businesses,","price":"$4500"},
+    {"ID":4 ,"Name":"Honda","Category":"BRV","":"","Image":"img.7.jpg","Specification":"Badminton is a type of indoor game which is played between either two teams or two players. It is one of the best games for an individual's physical fitness,","price":"$4500"},
+    {"ID":5 ,"Name":"Honda","Category":"iNSPIRE","":"","Image":"img.2a.jpg","Specification":"Honda was the first Japanese automobile manufacturer to release a dedicated luxury brand, Acura, in 1986. Aside from their core automobile and motorcycle businesses,","price":"$4500"},
+    {"ID":6 ,"Name":"Honda","Category":"ACCORD","":"","Image":"img.4.jpg","Specification":"Honda was the first Japanese automobile manufacturer to release a dedicated luxury brand, Acura, in 1986. Aside from their core automobile and motorcycle businesses,","price":"$4500"},
+    {"ID":6 ,"Name":"TOYOTA","Category":"COROLLA","":"","Image":"img.5.jpg","Specification":"Honda was the first Japanese automobile manufacturer to release a dedicated luxury brand, Acura, in 1986. Aside from their core automobile and motorcycle businesses,","price":"$4500"},
+    {"ID":6 ,"Name":"TOYOTA","Category":"CROSS","":"","Image":"img.6.jpg","Specification":"Honda was the first Japanese automobile manufacturer to release a dedicated luxury brand, Acura, in 1986. Aside from their core automobile and motorcycle businesses,","price":"$4500"},
+    {"ID":6 ,"Name":"TOYOTA","Category":"4RUNNER","":"","Image":"img.9.webp","Specification":"Honda was the first Japanese automobile manufacturer to release a dedicated luxury brand, Acura, in 1986. Aside from their core automobile and motorcycle businesses,","price":"$4500"},
+    {"ID":6 ,"Name":"TOYOTA","Category":"ALTIS","":"","Image":"images.jfif","Specification":"Honda was the first Japanese automobile manufacturer to release a dedicated luxury brand, Acura, in 1986. Aside from their core automobile and motorcycle businesses,","price":"$4500"},
+    {"ID":6 ,"Name":"TOYOTA","Category":"VIOS","":"","Image":"download.jfif","Specification":"Honda was the first Japanese automobile manufacturer to release a dedicated luxury brand, Acura, in 1986. Aside from their core automobile and motorcycle businesses,","price":"$4500"},
+    {"ID":6 ,"Name":"TOYOTA","Category":"CROWN","":"","Image":"img.9.jpg","Specification":"Honda was the first Japanese automobile manufacturer to release a dedicated luxury brand, Acura, in 1986. Aside from their core automobile and motorcycle businesses,","price":"$4500"},
+]
+
+
+
+
+
+
+
+
